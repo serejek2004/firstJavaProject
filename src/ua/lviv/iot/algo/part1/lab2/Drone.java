dbf898e09da63b1297a725fa3ca43eee10a044fa
serejek2004 <serejek2004@gmail.com>
2023-03-25 01:03:32 +0200

    Upload laboratory work №2

---
+++ src/ua/lviv/iot/algo/part1/lab2/Drone.java
@@ -0,0 +1,49 @@
+package ua.lviv.iot.algo.part1.lab2;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Drone extends AerialVehicle {
+    private double currentBattaryLevelInPercent;
+    private double battaryCapacityInMA;
+
+    public double chargeBattary(double amount) {
+        if (currentBattaryLevelInPercent != 100) {
+            currentBattaryLevelInPercent += amount;
+        }
+        if (currentBattaryLevelInPercent > 100) {
+            System.out.println("Error");
+            return chargeBattary(amount);
+        }
+        System.out.println("Success");
+        return currentBattaryLevelInPercent;
+    }
+
+    public double useBattary(double amount) {
+        if (currentBattaryLevelInPercent != 0) {
+            currentBattaryLevelInPercent -= amount;
+        }
+        if (currentBattaryLevelInPercent == 1) {
+            System.out.println("Attantion drone will fall!!!");
+        }
+        if (currentBattaryLevelInPercent < 0) {
+            System.out.println("Error");
+            return useBattary(amount);
+        }
+        System.out.println("Success");
+        return currentBattaryLevelInPercent;
+    }
+
+    @Override
+    public void flyAt(double speedMetersPerMinute, double altitude) {
+        setCurrentSpeed(speedMetersPerMinute);
+        setCurrentAltitude(altitude);
+    }
+
+    public String toString() {
+        return "Drone - " + super.toString() + " currentBattaryLevel=" + currentBattaryLevelInPercent + "%, battaryCapacityInMA=" + battaryCapacityInMA;
+    }
+}
---
+++ src/ua/lviv/iot/algo/part1/lab2/Helicopter.java
@@ -0,0 +1,50 @@
+package ua.lviv.iot.algo.part1.lab2;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Helicopter extends AerialVehicle {
+    private double fuelCapicityInLiters;
+    private double currentFuelLevel;
+
+    public double refuel(double amount) {
+        if (currentFuelLevel != 100) {
+            currentFuelLevel += amount;
+        }
+        if (currentFuelLevel > 100) {
+            System.out.println("Error");
+            return refuel(amount);
+        }
+        System.out.println("Success");
+        return currentFuelLevel;
+    }
+
+    public double useFuel(double amount) {
+        if (currentFuelLevel != 0) {
+            currentFuelLevel -= amount;
+        }
+        if (currentFuelLevel == 1) {
+            System.out.println("Attantion you`ll fall!!!");
+        }
+        if (currentFuelLevel < 0) {
+            System.out.println("Error");
+            return useFuel(amount);
+        }
+        System.out.println("Success");
+        return currentFuelLevel;
+    }
+
+    @Override
+    public void flyAt(double speedMetersPerMinute, double altitude) {
+        setCurrentSpeed(speedMetersPerMinute);
+        setCurrentAltitude(altitude);
+    }
+
+    @Override
+    public String toString() {
+        return "Helicopter - " + super.toString() + " currentFuelLevel=" + currentFuelLevel + ", fuelCapicityInLiters=" + fuelCapicityInLiters;
+    }
+}
