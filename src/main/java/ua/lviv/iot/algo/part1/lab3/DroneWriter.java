6b3cf7ec8340c667dfb4b5b0e944474a10f7778d
serejek2004 <serejek2004@gmail.com>
2023-04-23 21:17:08 +0300

    Fix code (DroneWriter)

--- src/main/java/ua/lviv/iot/algo/part1/lab3/DroneWriter.java
+++ src/main/java/ua/lviv/iot/algo/part1/lab3/DroneWriter.java
@@ -2,19 +2,18 @@ package ua.lviv.iot.algo.part1.lab3;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DroneWriter {
     public void writeToFile(final List<AbstractDrone> droneList, final String fileName) throws FileNotFoundException {
         PrintWriter printer = new PrintWriter(fileName);
-        droneList.sort(Comparator.comparing(drone -> drone.getClass().getSimpleName()));
-        Map<Class<?>, List<AbstractDrone>> mapOfDrones = new HashMap<>();
 
-        for (AbstractDrone drone : droneList) {
-            mapOfDrones.computeIfAbsent(drone.getClass(), s -> new LinkedList<>()).add(drone);
-        }
+        Map<Class<? extends AbstractDrone>, List<AbstractDrone>> mapOfDrones = droneList.stream()
+                .collect(Collectors.groupingBy(AbstractDrone::getClass));
 
-        for (Map.Entry<Class<?>, List<AbstractDrone>> drone : mapOfDrones.entrySet()) {
+        for (Map.Entry<Class<? extends AbstractDrone>, List<AbstractDrone>> drone : mapOfDrones.entrySet()) {
             printer.println(drone.getKey().getSimpleName());
             printer.println(drone.getValue().get(0).getHeaders());
             for (AbstractDrone sameDrone : drone.getValue()) {
