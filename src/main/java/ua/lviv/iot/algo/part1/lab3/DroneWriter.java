d1d9d4553547bb0ef836697cf26189f159ba1b79
serejek2004 <serejek2004@gmail.com>
2023-04-25 11:36:45 +0300

    Update DroneWriter & fix tests

--- src/main/java/ua/lviv/iot/algo/part1/lab3/DroneWriter.java
+++ src/main/java/ua/lviv/iot/algo/part1/lab3/DroneWriter.java
@@ -2,25 +2,27 @@ package ua.lviv.iot.algo.part1.lab3;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class DroneWriter {
     public void writeToFile(final List<AbstractDrone> droneList, final String fileName) throws FileNotFoundException {
         PrintWriter printer = new PrintWriter(fileName);
 
-        Map<Class<? extends AbstractDrone>, List<AbstractDrone>> mapOfDrones = droneList.stream()
-                .collect(Collectors.groupingBy(AbstractDrone::getClass));
+        droneList.sort(Comparator.comparing(o -> o.getClass().getName()));
 
-        for (Map.Entry<Class<? extends AbstractDrone>, List<AbstractDrone>> drone : mapOfDrones.entrySet()) {
-            printer.println(drone.getKey().getSimpleName());
-            printer.println(drone.getValue().get(0).getHeaders());
-            for (AbstractDrone sameDrone : drone.getValue()) {
-                printer.println(sameDrone.toCSV());
+        String temporaryName = null;
+
+        for (AbstractDrone drone : droneList) {
+            if (temporaryName != drone.getClass().getName()) {
+                printer.println(drone.getClass().getSimpleName());
+                printer.println(drone.getHeaders());
             }
-        }
 
+            printer.println(drone.toCSV());
+            temporaryName = drone.getClass().getName();
+
+        }
         printer.close();
     }
 }
